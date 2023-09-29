cf6a203245e9fb076e60872c772f3763276a0184
EWire-hub <tyudai81@gmail.com>
2023-09-29 21:06:20 +0900

    first commit

--- static/target/debug/deps/mylib-008229e52247e911.d
+++
@@ -1,5 +0,0 @@
-/home/ewire/competitive/rust/cp_library_rs/static/target/debug/deps/mylib-008229e52247e911.rmeta: mylib/src/lib.rs
-
-/home/ewire/competitive/rust/cp_library_rs/static/target/debug/deps/mylib-008229e52247e911.d: mylib/src/lib.rs
-
-mylib/src/lib.rs:
