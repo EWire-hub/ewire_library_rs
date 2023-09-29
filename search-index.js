c3a4342cd819300e3814dc36495d7edc1eecfc29
EWire-hub <EWire-hub@users.noreply.github.com>
2023-09-29 16:26:02 +0000

    deploy: 1c9ac6b98ee277f98ad30ae83342aa789ea445e9

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"cp_library_rs":{"doc":"競プロ用ライブラリ","t":"AFF","n":["math_number_theory","add","is_prime"],"q":[[0,"cp_library_rs"],[1,"cp_library_rs::math_number_theory::is_prime"]],"d":["数学関連のアルゴリズム","",""],"i":[0,0,0],"f":[0,[[1,1],1],[2,3]],"c":[],"p":[[15,"usize"],[15,"i64"],[15,"bool"]]}\
+"cp_library_rs":{"doc":"競プロ用ライブラリ","t":"AFF","n":["math_number_theory","add","is_prime_"],"q":[[0,"cp_library_rs"],[1,"cp_library_rs::math_number_theory::is_prime"]],"d":["数学関連のアルゴリズム","",""],"i":[0,0,0],"f":[0,[[1,1],1],[2,3]],"c":[],"p":[[15,"usize"],[15,"i64"],[15,"bool"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
