4b874e970c6a4a658acb88c488455d4849885bfc
EWire-hub <EWire-hub@users.noreply.github.com>
2023-09-29 09:12:32 +0000

    deploy: 2495e868fa5416821e5eaf285990d8fe0c2011c3

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"mylib":{"doc":"","t":"F","n":["add"],"q":[[0,"mylib"]],"d":["競プロ用ライブラリ"],"i":[0],"f":[[[1,1],1]],"c":[],"p":[[15,"usize"]]}\
+"mylib":{"doc":"競プロ用ライブラリ","t":"F","n":["add"],"q":[[0,"mylib"]],"d":[""],"i":[0],"f":[[[1,1],1]],"c":[],"p":[[15,"usize"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
