048c8317a4306c87020fdd3a53a0f634dc94914a
EWire-hub <tyudai81@gmail.com>
2023-09-29 17:16:41 +0900

    test

---
+++ static/target/debug/deps/mylib-008229e52247e911.d
@@ -0,0 +1,5 @@
+/home/ewire/competitive/rust/cp-library-rs/static/target/debug/deps/mylib-008229e52247e911.rmeta: mylib/src/lib.rs
+
+/home/ewire/competitive/rust/cp-library-rs/static/target/debug/deps/mylib-008229e52247e911.d: mylib/src/lib.rs
+
+mylib/src/lib.rs:
---
+++ static/target/debug/deps/mylib-075a276bad27299b.d
@@ -0,0 +1,5 @@
+/home/ewire/competitive/rust/cp-library-rs/static/target/debug/deps/mylib-075a276bad27299b.rmeta: mylib/src/lib.rs
+
+/home/ewire/competitive/rust/cp-library-rs/static/target/debug/deps/mylib-075a276bad27299b.d: mylib/src/lib.rs
+
+mylib/src/lib.rs:
