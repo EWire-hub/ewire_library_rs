d47d929cd7dd88158f33bb27f6f995d3d8bb2ffa
EWire-hub <EWire-hub@users.noreply.github.com>
2024-03-03 17:17:08 +0000

    deploy: f60f573f63494d292ee46bab602f7bc347abbd0c

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
-var searchIndex = JSON.parse('{\
-"cp_library_rs":{"doc":"競プロ用ライブラリ","t":"AFF","n":["math_number_theory","add","is_prime"],"q":[[0,"cp_library_rs"],[1,"cp_library_rs::math_number_theory::is_prime_"]],"d":["数学関連のアルゴリズム","",""],"i":[0,0,0],"f":[0,[[1,1],1],[2,3]],"c":[],"p":[[15,"usize"],[15,"i64"],[15,"bool"]]}\
-}');
-if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
-if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
+var searchIndex = new Map(JSON.parse('[\
+["cp_library_rs",{"doc":"競プロ用ライブラリ","t":"CHH","n":["math_number_theory","add","is_prime"],"q":[[0,"cp_library_rs"],[1,"cp_library_rs::math_number_theory::is_prime_"]],"d":["数学関連のアルゴリズム","",""],"i":[0,0,0],"f":[0,[[1,1],1],[2,3]],"c":[],"p":[[1,"usize"],[1,"i64"],[1,"bool"]],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["cp_library_rs",["",[],["lib.rs","math_number_theory.rs"]]]\
+]'));
+createSrcSidebar();
